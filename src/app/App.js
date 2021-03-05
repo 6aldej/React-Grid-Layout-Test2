ae2cce8557f1d12144b2a164ef13b253f731b841
6aldej <gruffbarbecue@gmail.com>
2021-03-05 16:05:41 +0300

    ololo

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react';
 import DashboardList from '../component/DashboardList/DashboardList';
+import axios from 'axios';
 
 export default class App extends Component {
+  state = {
+    data: [],
+  }
+
+  log() {
+    axios.get('http://localhost:3000/search-title/nodejs')
+      .then((res) => {
+        const result = res.data.hits.hits[0];
+        this.setState({ data: result });
+      });
+  }
+
+  componentDidMount() {
+    this.log()
+    
+  }
 
   render() {
+    console.log(this.state.data)
     return (
       <div className="App">
         <DashboardList/>
       </div>
+      
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import '../node_modules/react-grid-layout/css/styles.css';
 import '../node_modules/react-resizable/css/styles.css';
 import App from './app/App'
 
+
+
 ReactDOM.render(<App />,document.getElementById('root'));
